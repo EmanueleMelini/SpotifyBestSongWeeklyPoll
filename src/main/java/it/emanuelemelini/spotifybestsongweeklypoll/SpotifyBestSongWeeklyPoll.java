d659af783422e40462f174ef144e261ad9a261d0
Emanuele Melini <meliniemanuele@gmail.com>
2021-10-08 01:00:56 +0200

    Fixed HTTP Call to Spotify API

--- src/main/java/it/emanuelemelini/spotifybestsongweeklypoll/SpotifyBestSongWeeklyPoll.java
+++ src/main/java/it/emanuelemelini/spotifybestsongweeklypoll/SpotifyBestSongWeeklyPoll.java
@@ -1,13 +1,20 @@
 package it.emanuelemelini.spotifybestsongweeklypoll;
 
-import com.wrapper.spotify.SpotifyApi;
-import com.wrapper.spotify.model_objects.specification.Paging;
-import com.wrapper.spotify.model_objects.specification.PlaylistTrack;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import reactor.core.publisher.Mono;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SpotifyBestSongWeeklyPoll {
 
@@ -49,19 +56,55 @@ public class SpotifyBestSongWeeklyPoll {
 									playlist = command[1];
 									token = command[2];
 
-									SpotifyApi spotifyApi = new SpotifyApi.Builder().setAccessToken(token)
-											.build();
-
 									try {
-										//TODO: resolve track name error
-										Paging<PlaylistTrack> playtrack = spotifyApi.getPlaylistsItems(playlist)
-												.fields("items(added_by.id,track.name)")
-												.build()
-												.execute();
+										//.id,added_by.displayname
 
-										System.out.println(playtrack);
-									} catch(Exception e) {
-										System.out.println(e.getMessage());
+										//
+
+										URL url = new URL("https://api.spotify.com/v1/playlists/" + playlist + "/tracks?fields=items(added_by.id,track.name)");
+										HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+										conn.setRequestProperty("Authorization", "Bearer " + token);
+
+										conn.setRequestProperty("Content-Type", "application/json");
+										conn.setRequestMethod("GET");
+
+
+										BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+										String output;
+										Set<String> ids = new HashSet<>();
+										JSONArray array;
+										Set<String> names = new HashSet<>();
+
+										StringBuffer response = new StringBuffer();
+										while((output = in.readLine()) != null) {
+											response.append(output);
+										}
+
+										in.close();
+										System.out.println(response);
+										String str = response.toString().replace(" ", "");
+										JSONObject object = new JSONObject(str);
+										array = object.getJSONArray("items");
+										for(int i = 0; i < array.length(); i++) {
+											ids.add(array.getJSONObject(i).getJSONObject("added_by").getString("id"));
+											names.add(array.getJSONObject(i).getJSONObject("track").getString("name"));
+										}
+
+										String messObj = "";
+
+										for(int i = 0; i < ids.size(); i++) {
+											messObj = messObj + ids.toArray()[i] + ":" + names.toArray()[i] + "\n";
+										}
+
+										final String messagefinal = messObj;
+
+										//TODO: better message and poll
+
+										return message.getChannel().flatMap(messageChannel -> messageChannel.createMessage(messagefinal));
+
+									} catch(IOException e) {
+										System.out.printf("IO " + e.getMessage());
 									}
 
 								}
