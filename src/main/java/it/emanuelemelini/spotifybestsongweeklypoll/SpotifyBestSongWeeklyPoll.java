b0a9a150247ca04f3f68546364b737f0987eacac
Emanuele Melini <emanuele.melini01@gmail.com>
2021-10-08 15:38:26 +0200

    Fixed message output

--- src/main/java/it/emanuelemelini/spotifybestsongweeklypoll/SpotifyBestSongWeeklyPoll.java
+++ src/main/java/it/emanuelemelini/spotifybestsongweeklypoll/SpotifyBestSongWeeklyPoll.java
@@ -9,12 +9,14 @@ import org.json.JSONObject;
 import reactor.core.publisher.Mono;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SpotifyBestSongWeeklyPoll {
 
@@ -41,11 +43,9 @@ public class SpotifyBestSongWeeklyPoll {
 								String[] command = message.getContent()
 										.split(" ");
 
-								//TODO: auto token
-
-
 								String playlist;
-								String token;
+								String clientID;
+								String clientIDSecret;
 
 								if(command[0].equalsIgnoreCase("!contest")) {
 
@@ -54,17 +54,85 @@ public class SpotifyBestSongWeeklyPoll {
 												.flatMap(messageChannel -> messageChannel.createMessage("Insert playlist id!"));
 
 									playlist = command[1];
-									token = command[2];
+									clientID = command[2];
+									clientIDSecret = command[3];
 
 									try {
-										//.id,added_by.displayname
 
-										//
+										String paramsLogin = "grant_type=client_credentials";
+										byte[] postDataLogin = paramsLogin.getBytes();
+										String clientLoginTo64 = clientID + ":" + clientIDSecret;
+										byte[] accessLogin = clientLoginTo64.getBytes();
+										int postDataLenghtLogin = postDataLogin.length;
+										URL urlLogin = new URL("https://accounts.spotify.com/api/token");
+										HttpURLConnection connLogin = (HttpURLConnection) urlLogin.openConnection();
+										connLogin.setDoOutput(true);
+										connLogin.setRequestMethod("POST");
+										String base64 = Base64.getEncoder()
+												.encodeToString(accessLogin);
+										System.out.println("Base: " + base64);
+										connLogin.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+										connLogin.setRequestProperty("Authorization", "Basic " + base64);
+										connLogin.setRequestProperty("charset", "utf-8");
+										connLogin.setRequestProperty("Content-Lenght", Integer.toString(postDataLenghtLogin));
+										try(DataOutputStream wr = new DataOutputStream(connLogin.getOutputStream())) {
+											wr.write(postDataLogin);
+										}
+										BufferedReader inLogin = new BufferedReader(new InputStreamReader(connLogin.getInputStream()));
+										String outputLogin;
 
-										URL url = new URL("https://api.spotify.com/v1/playlists/" + playlist + "/tracks?fields=items(added_by.id,track.name)");
+										StringBuffer responseLogin = new StringBuffer();
+										while((outputLogin = inLogin.readLine()) != null) {
+											responseLogin.append(outputLogin);
+										}
+										System.out.println("Login: " + responseLogin);
+										inLogin.close();
+
+										String strLogin = responseLogin.toString()
+												.replace(":", ": ");
+										System.out.println(strLogin);
+										JSONObject objectLogin = new JSONObject(strLogin);
+										System.out.println(objectLogin);
+										String accessToken = objectLogin.getString("access_token");
+										System.out.println("Token: " + accessToken);
+
+										/*
+
+
+										String params = "grant_type=refresh_token&refresh_token=" + token;
+										byte[] postData = params.getBytes(StandardCharsets.UTF_8);
+										String clientsIDs = clientID + ":" + clientIDSecret;
+										byte[] access = clientsIDs.getBytes(StandardCharsets.UTF_8);
+										int postDataLenght = postData.length;
+										URL urlRefresh = new URL("https://accounts.spotify.com/api/token");
+										HttpURLConnection connRefresh = (HttpURLConnection) urlRefresh.openConnection();
+										connRefresh.setDoOutput(true);
+										connRefresh.setRequestMethod("POST");
+										connRefresh.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+										connRefresh.setRequestProperty("Authorization", "Basic " +
+												java.util.Base64.getEncoder().encodeToString(access));
+										connRefresh.setRequestProperty("charset", "utf-8");
+										connRefresh.setRequestProperty("Content-Lenght", Integer.toString(postDataLenght));
+										try(DataOutputStream wr = new DataOutputStream(connRefresh.getOutputStream())) {
+											wr.write(postData);
+										}
+										BufferedReader inRefresh = new BufferedReader(new InputStreamReader(connRefresh.getInputStream()));
+										String outputRefresh;
+
+										StringBuffer responseRefresh = new StringBuffer();
+										while((outputRefresh = inRefresh.readLine()) != null) {
+											responseRefresh.append(outputRefresh);
+										}
+										System.out.println("Refresh: " + responseRefresh);
+										inRefresh.close();
+										*/
+
+
+										URL url = new URL("https://api.spotify.com/v1/playlists/" + playlist +
+												"/tracks?fields=items(added_by.id,track.name)");
 										HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-										conn.setRequestProperty("Authorization", "Bearer " + token);
+										conn.setRequestProperty("Authorization", "Bearer " + accessToken);
 
 										conn.setRequestProperty("Content-Type", "application/json");
 										conn.setRequestMethod("GET");
@@ -72,9 +140,8 @@ public class SpotifyBestSongWeeklyPoll {
 
 										BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 										String output;
-										Set<String> ids = new HashSet<>();
 										JSONArray array;
-										Set<String> names = new HashSet<>();
+										Map<String, String> mapR = new HashMap<>();
 
 										StringBuffer response = new StringBuffer();
 										while((output = in.readLine()) != null) {
@@ -82,29 +149,71 @@ public class SpotifyBestSongWeeklyPoll {
 										}
 
 										in.close();
+
 										System.out.println(response);
-										String str = response.toString().replace(" ", "");
+
+										String str = response.toString()
+												.replace(" ", "");
+
 										JSONObject object = new JSONObject(str);
+
 										array = object.getJSONArray("items");
+
 										for(int i = 0; i < array.length(); i++) {
-											ids.add(array.getJSONObject(i).getJSONObject("added_by").getString("id"));
-											names.add(array.getJSONObject(i).getJSONObject("track").getString("name"));
+											mapR.put(array.getJSONObject(i)
+															.getJSONObject("added_by")
+															.getString("id"),
+													array.getJSONObject(i)
+															.getJSONObject("track")
+															.getString("name"));
 										}
 
-										String messObj = "";
+										StringBuilder messObj = new StringBuilder();
 
-										for(int i = 0; i < ids.size(); i++) {
-											messObj = messObj + ids.toArray()[i] + ":" + names.toArray()[i] + "\n";
-										}
+										mapR.forEach((id, name) -> {
 
-										final String messagefinal = messObj;
+											try {
 
+												URL urlUser = new URL(
+														"https://api.spotify.com/v1/users/" + id);
+												HttpURLConnection connectionUser = (HttpURLConnection) urlUser.openConnection();
+												connectionUser.setRequestProperty("Content-Type", "application/json");
+												connectionUser.setRequestProperty("Authorization",
+														"Bearer " + accessToken);
+												connectionUser.setRequestMethod("GET");
+												BufferedReader inUser = new BufferedReader(new InputStreamReader(
+														connectionUser.getInputStream()));
+
+												String outputUser;
+
+												StringBuffer responseUser = new StringBuffer();
+												while((outputUser = inUser.readLine()) != null) {
+													responseUser.append(outputUser);
+												}
+												inUser.close();
+												System.out.println(responseUser);
+
+												String nameUser = new JSONObject(responseUser.toString()
+														.replace(":", ": ")).getString("display_name");
+
+												messObj.append(nameUser)
+														.append(":")
+														.append(name)
+														.append("\n");
+
+											}catch(Exception e) {
+												System.out.println(e.getMessage());
+											}
+										});
+
+										final String messagefinal = messObj.toString();
 										//TODO: better message and poll
 
-										return message.getChannel().flatMap(messageChannel -> messageChannel.createMessage(messagefinal));
+										return message.getChannel()
+												.flatMap(messageChannel -> messageChannel.createMessage(messagefinal));
 
 									} catch(IOException e) {
-										System.out.printf("IO " + e.getMessage());
+										System.out.println("IO " + e.getMessage());
 									}
 
 								}
