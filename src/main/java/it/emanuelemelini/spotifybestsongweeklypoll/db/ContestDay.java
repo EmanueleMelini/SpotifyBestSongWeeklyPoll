845bab9858866e003071e4503758d00a17ed3562
Emanuele Melini <emanuele.melini01@gmail.com>
2021-10-15 15:43:59 +0200

    Changed Entity name in PascalCase and Attributes name in lowercase

--- src/main/java/it/emanuelemelini/spotifybestsongweeklypoll/db/ContestDay.java
+++ src/main/java/it/emanuelemelini/spotifybestsongweeklypoll/db/ContestDay.java
@@ -4,37 +4,39 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "contest_day")
-public class Contest_day {
+public class ContestDay {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.TABLE)
-	private long idcontest_day;
-	private String guild_id;
+	@Column(name = "idcontest_day")
+	private long idcontestday;
+	@Column(name = "guild_id")
+	private String guildid;
 	private Day day;
 	private boolean deleted;
 
-	protected Contest_day() {}
+	protected ContestDay() {}
 
-	public Contest_day(String guild_id, Day day, boolean deleted) {
-		this.guild_id = guild_id;
+	public ContestDay(String guildid, Day day, boolean deleted) {
+		this.guildid = guildid;
 		this.day = day;
 		this.deleted = deleted;
 	}
 
-	public long getIdcontest_day() {
-		return idcontest_day;
+	public long getIdcontestday() {
+		return idcontestday;
 	}
 
-	public void setIdcontest_day(long idcontest_day) {
-		this.idcontest_day = idcontest_day;
+	public void setIdcontestday(long idcontest_day) {
+		this.idcontestday = idcontest_day;
 	}
 
-	public String getGuild_id() {
-		return guild_id;
+	public String getGuildid() {
+		return guildid;
 	}
 
-	public void setGuild_id(String guild_id) {
-		this.guild_id = guild_id;
+	public void setGuildid(String guild_id) {
+		this.guildid = guild_id;
 	}
 
 	public Day getDay() {
---
+++ src/main/java/it/emanuelemelini/spotifybestsongweeklypoll/db/ContestDayRepository.java
@@ -0,0 +1,16 @@
+package it.emanuelemelini.spotifybestsongweeklypoll.db;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ContestDayRepository extends CrudRepository<ContestDay, Long> {
+
+	List<ContestDay> getAllContestDay();
+
+	List<ContestDay> getAllContestDayByGuildid(String guildid);
+
+	List<ContestDay> getAllContestDayByDay(ContestDay.Day day);
+
+	ContestDay getContestDayByIdcontestday(long IDcontestday);
+}
